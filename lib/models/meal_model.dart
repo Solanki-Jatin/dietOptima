a7bb35161dc384371a6e9315acc13ba6a5762c4c
Jatin Solanki <jatinsolanki1502@gmail.com>
2026-04-14 23:38:42 +0530

    Add files via upload

---
+++ lib/models/meal_model.dart
@@ -0,0 +1,13 @@
+class MealModel {
+  final String name;
+  final double calories;
+  final double protein;
+  final DateTime time;
+
+  MealModel({
+    required this.name,
+    required this.calories,
+    required this.protein,
+    required this.time,
+  });
+}
